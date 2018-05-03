b211e3d6713632dfd2d63a42c2f42bfae160deea
dhruv-chitkara27 <32064824+dhruv-chitkara27@users.noreply.github.com>
2018-05-03 23:15:14 +0530

    Add files via upload

---
+++ prize.py
@@ -0,0 +1,16 @@
+def which_prize(points):
+    """
+    Returns the number of prize-winning message, given a number of points
+    """
+    prize = None
+    if points <= 50:
+        prize = "a wooden rabbit"
+    elif 151 <= points <= 180:
+        prize = "a wafer-thin mint"
+    elif points >= 181:
+        prize = "a penguin"
+
+    if prize:
+        return "Congratulations! You have won " + prize + "!"
+    else:
+        return "Oh dear, no prize this time."
